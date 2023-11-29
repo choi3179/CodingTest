058630aec6753f6bcf4835414dc97f393330c4cb
Choi WonKyu <66417882+choi3179@users.noreply.github.com>
2023-11-29 11:16:39 +0900

    [Bronze I] Title: 세로읽기, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char[][] words = new char[5][15];
+
+        for(int i=0;i<words.length;i++){
+            String word = sc.nextLine();
+            for(int j=0;j<word.length();j++){
+                words[i][j] = word.charAt(j);
+            }
+        }
+
+        for(int i=0;i<15;i++){
+            for(int j=0;j<words.length;j++){
+                if(words[j][i] != '\0')
+                    System.out.print(words[j][i]);
+            }
+        }
+
+    }
+}
