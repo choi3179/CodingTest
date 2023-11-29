8ecf89e68b121dd510a9b686428b262997f1c893
Choi WonKyu <66417882+choi3179@users.noreply.github.com>
2023-11-29 17:20:01 +0900

    [Silver V] Title: 색종이, Time: 224 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int[][] area = new int[100][100];
+
+        n = sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            int x= sc.nextInt();
+            int y= sc.nextInt();
+
+            for(int j=x;j<x+10;j++){
+                for(int k=y;k<y+10;k++){
+                    area[j][k] = 1;
+                }
+            }
+        }
+
+        int sum = 0;
+
+        for(int i=0;i<area.length;i++){
+            for(int j=0;j<area[i].length;j++){
+                sum += area[i][j];
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
