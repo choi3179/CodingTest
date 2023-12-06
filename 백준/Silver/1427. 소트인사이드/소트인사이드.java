7a190bf477ea3505c70251c93ff2b97813a4e7d5
Choi WonKyu <66417882+choi3179@users.noreply.github.com>
2023-12-06 15:10:55 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bf.readLine();
+        ArrayList<String> arr = new ArrayList<String>(List.of(input.split("")));
+
+        Collections.sort(arr,Collections.reverseOrder());
+
+        for(String k: arr)
+            System.out.print(k);
+    }
+}
