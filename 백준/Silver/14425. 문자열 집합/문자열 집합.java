7e78b51aa6dd1b426be51414a5a5c03bff965d72
Choi WonKyu <66417882+choi3179@users.noreply.github.com>
2023-12-11 11:38:10 +0900

    [Silver IV] Title: 문자열 집합, Time: 2812 ms, Memory: 31084 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        String[] spl = br.readLine().split(" ");
+
+        int m = Integer.parseInt(spl[0]);
+        int n = Integer.parseInt(spl[1]);
+        int ans = 0;
+
+        String[] check = new String[m];
+        for(int i=0;i<m;i++){
+            check[i] = br.readLine();
+        }
+
+        //String[] patterns = new String[n];
+        for(int i=0;i<n;i++){
+            String patterns = br.readLine().trim();
+            for(int j=0;j<m;j++){
+                if(check[j].equals(patterns)){
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
